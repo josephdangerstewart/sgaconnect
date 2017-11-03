86d999d067237b75e769d74acf5c7fe8ea06c438
Ashley Cheah <ashleyc21@AD.BIOLA.EDU>
2017-11-02 17:49:30 -0700

    began working on icons for header

--- src/sgaconnect/Header.java
+++ src/sgaconnect/Header.java
@@ -27,21 +27,67 @@ public class Header extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jLabel1 = new javax.swing.JLabel();
+        sgaLogoHomeButton = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
         setBackground(new java.awt.Color(163, 42, 52));
+
+        sgaLogoHomeButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/sgaconnect/resources/Images/SGALOGOsmaller.png"))); // NOI18N
+        sgaLogoHomeButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                sgaLogoHomeButtonMouseClicked(evt);
+            }
+        });
+
+        jLabel2.setFont(new java.awt.Font("Steelfish Outline", 1, 48)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(255, 251, 234));
+        jLabel2.setText("SGA Connect");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 964, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(35, 35, 35)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 660, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(sgaLogoHomeButton, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 102, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(sgaLogoHomeButton)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(39, 39, 39)
+                            .addComponent(jLabel1))
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(21, 21, 21)
+                            .addComponent(jLabel2))))
+                .addContainerGap(28, Short.MAX_VALUE))
         );
+
+        sgaLogoHomeButton.getAccessibleContext().setAccessibleName("Home");
     }// </editor-fold>//GEN-END:initComponents
+
+    private void sgaLogoHomeButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_sgaLogoHomeButtonMouseClicked
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_sgaLogoHomeButtonMouseClicked
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox<String> jComboBox1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel sgaLogoHomeButton;
     // End of variables declaration//GEN-END:variables
 }
