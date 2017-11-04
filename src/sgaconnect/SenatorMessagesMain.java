2cc4c9e1787900ce0579948c48379bde7565fd5f
Joseph Stewart <josephs12@ad.biola.edu>
2017-11-04 14:23:16 -0700

    Removed AutocompleteDecorator which was causing some problems

--- src/sgaconnect/SenatorMessagesMain.java
+++ src/sgaconnect/SenatorMessagesMain.java
@@ -11,7 +11,6 @@ import javax.swing.JComboBox;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
-import org.jdesktop.swingx.autocomplete.AutoCompleteDecorator;
 import org.json.JSONObject;
 
 /**
@@ -37,8 +36,6 @@ public class SenatorMessagesMain extends javax.swing.JPanel {
     }
     
     public void init() {
-        //JComboBox comboBox = new JComboBox();
-        AutoCompleteDecorator.decorate(StudentNames);
     }
     
     /**
